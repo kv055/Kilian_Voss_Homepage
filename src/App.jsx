2fd39838ad8c6e9124e132bfc01285c70e668e00
Kilian Voss <kilian96@live.de>
2023-02-09 17:27:07 +1100

    Init commit for a new vite project of my homepage

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+
+const Layout = () => {
+  return (
+    <div>
+      <header>
+        <nav>Header Nav</nav>
+        <h1>Header Title</h1>
+      </header>
+      <nav>Main Nav</nav>
+      <main>
+        <section>Section 1</section>
+        <section>Section 2</section>
+        <section>Section 3</section>
+      </main>
+      <aside>Aside</aside>
+      <footer>
+        <nav>Footer Nav</nav>
+        <p>Footer Content</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
+
+// Homepage: Your homepage should give a brief introduction about yourself and what you do, your skills and expertise. It can also include a high-level overview of your portfolio.
+
+// About Me: This section should provide a more detailed introduction about yourself, your experience, education, and interests.
+
+// Portfolio: This is the most important section of your website, where you showcase your finished projects. You can organize your portfolio by categories, for example, web development, mobile development, and so on. You can include screenshots, descriptions, and links to live demos of your projects.
+
+// Services: This section can describe the services you offer, such as web development, consulting, and training.
+
+// Blog: This section is optional, but it can be a good place to showcase your thoughts and ideas, write about your projects, and share updates on your work.
+
+// Contact: This section should include your contact information, such as your email address, phone number, and links to your social media profiles.
