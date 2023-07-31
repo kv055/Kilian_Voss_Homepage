acb348b0b1b3eb6153a3b16fab2de28c4fb5cccc
Kilian Voss <kilian96@live.de>
2023-07-31 23:26:23 +1000

    set up remote service worker

--- src/components/pdf_viewer.jsx
+++ src/components/pdf_viewer.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Document, Page } from 'react-pdf';
+import React from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const PDFViewer = ({ PdfDoc }) => {
   return (
     <div>
       <Document file={PdfDoc}>
-        <Page pageNumber={1} />
+        <Page pageNumber={1} scale={0.5} />
       </Document>
     </div>
   );
