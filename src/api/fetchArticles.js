0c14a6df5946b57b1c695c6c769bc5dcfce5fdaf
Kilian Voss <kilian96@live.de>
2023-08-15 15:57:18 +1000

    module that will in the future fetch content from the backend, returns mock data for now

---
+++ src/api/fetchArticles.js
@@ -0,0 +1,45 @@
+export function fetchArticles() {
+  let fetchedArticles = [
+    {
+      Title: {},
+      Icons: [],
+      Description: {},
+      Url: {},
+      Article: {},
+      Background: none,
+    },
+    {
+      Title: {},
+      Icons: [],
+      Description: {},
+      Url: {},
+      Article: {},
+      Background: none,
+    },
+    {
+      Title: {},
+      Icons: [],
+      Description: {},
+      Url: {},
+      Article: {},
+      Background: none,
+    },
+    {
+      Title: {},
+      Icons: [],
+      Description: {},
+      Url: {},
+      Article: {},
+      Background: none,
+    },
+    {
+      Title: {},
+      Icons: [],
+      Description: {},
+      Url: {},
+      Article: {},
+      Background: none,
+    },
+  ];
+  return fetchedArticles;
+}
