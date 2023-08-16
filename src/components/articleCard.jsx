44bb7f1a9474905b91087f91704b72d9ae169e98
Kilian Voss <kilian96@live.de>
2023-08-16 16:04:42 +1000

    added keys tomapped items, and new prameters  (state variables)

--- src/components/articleCard.jsx
+++ src/components/articleCard.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import "../index.css";
 import ExpandedArticle from "../components/expandedArticle";
 export default function ArticleCard({
-  ExpansionState,
+  isExpanded,
+  onExpand,
+  Id,
   Title,
   Description,
   Icons,
@@ -10,13 +12,10 @@ export default function ArticleCard({
   Url,
   Background,
 }) {
-  let [isExpanded, setIsExpanded] = useState(false);
-  const handleExpansion = () => {
-    setIsExpanded(!isExpanded);
-  };
   return (
     <>
       <div
+        
         className={`group relative ${
           isExpanded ? "col-span-full order-first" : "col-span-2 lg:col-span-1"
         } bg-white rounded-xl p-5 mb-5`}
@@ -25,7 +24,7 @@ export default function ArticleCard({
           {isExpanded && (
             <button
               className="visible col-span-2 p-2 border-black text-black border-solid border-4 rounded-xl Orbitron"
-              onClick={handleExpansion}
+              onClick={onExpand}
             >
               Close
             </button>
@@ -36,8 +35,8 @@ export default function ArticleCard({
           </h3>
 
           <div className="grid grid-flow-col col-span-2 md:col-span-1">
-            {Icons.map((item) => (
-              <img src={item} className=" w-8 h-8 mx-3" />
+            {Icons.map((item, index) => (
+              <img key={index} src={item} className=" w-8 h-8 mx-3" />
             ))}
           </div>
 
@@ -53,7 +52,7 @@ export default function ArticleCard({
         {!isExpanded && (
           <button
             className="visible absolute p-2 bg-blue-500 text-white top-1/2 left-1/2 rounded-xl Orbitron transform -translate-x-1/2 -translate-y-1/2 flex space-x-4 transition-opacity opacity-0 group-hover:opacity-100"
-            onClick={handleExpansion}
+            onClick={onExpand}
           >
             Expand
           </button>
