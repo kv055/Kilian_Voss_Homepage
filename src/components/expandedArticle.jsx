cdf33b623e748479f85f7d246fe7ffa52afb78d4
Kilian Voss <kilian96@live.de>
2023-08-16 16:05:02 +1000

    fixed responsivenes for smaller screens

--- src/components/expandedArticle.jsx
+++ src/components/expandedArticle.jsx
@@ -4,22 +4,22 @@ import "../index.css";
 export default function ExpandedArticle({Url, Article}) {
   return (
     <>
-      <div className="m-5 rounded-xl">
+      <div className="md:m-5 rounded-xl">
         <div className="flex flex-row flex-wrap justify-evenly">
           <div className="flex flex-row ">
             <a
               href={Url}
-              className="Orbitron text-lg tracking-wide font-bold"
+              className="Orbitron text-lg tracking-wide font-bold mt-5"
             >
               Visit: {Url}
             </a>
           </div>
         </div>
 
-        <h4 className="Orbitron text-lg tracking-wide font-bold">
+        <h4 className="m-5 Orbitron text-lg tracking-wide font-bold">
           The Process:
         </h4>
-        <p className="p-5 leading-9 tracking-wide text-lg text-left">{Article}</p>
+        <p className="pt-5 leading-7 md:leading-9 tracking-wide text-lg text-left">{Article}</p>
       </div>
     </>
   );
