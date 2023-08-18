9ec16d57f33e5ef2394a73128bca1d68a7dce566
Kilian Voss <kilian96@live.de>
2023-08-18 13:18:20 +1000

    PDF interactivity draft

--- src/components/ProfilepagePicCV.jsx
+++ src/components/ProfilepagePicCV.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import EpandedPDF from "../components/expandedPDF";
 import DownloadIcon from "../assets/icons/Download_icon.svg";
 import PreviewIcon from "../assets/icons/Preview_icon.svg";
-import ForwardArrowIcon from "../assets/icons/Forward_arrow_icon.svg";
-import BackwardArrowIcon from "../assets/icons/Backward_arrow_icon.svg";
 // import CV from "../assets/CV23.pdf";
 import CV from "../assets/KV_CV23.pdf";
 
@@ -22,116 +21,72 @@ const PicCV = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      if (isExpanded) {
-        setScale(window.innerWidth / 1000); // Adjust the divisor as per your needs
+      if (window.innerWidth < 640) {
+        // Tailwind's sm
+        setScale(0.4);
+      } else if (window.innerWidth < 768) {
+        // Tailwind's md
+        setScale(0.5);
+      } else if (window.innerWidth < 1024) {
+        // Tailwind's lg
+        setScale(0.5);
       } else {
-        if (window.innerWidth < 640) { // Tailwind's sm
-          setScale(0.4);
-        } else if (window.innerWidth < 768) { // Tailwind's md
-          setScale(0.5);
-        } else if (window.innerWidth < 1024) { // Tailwind's lg
-          setScale(0.6);
-        } else { // Tailwind's xl
-          setScale(0.7);
-        }
+        // Tailwind's xl
+        setScale(0.6);
       }
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize(); // Call the function initially to set the scale based on the initial viewport size
 
-    return () => window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [isExpanded]);
 
   return (
-    <div
-      className={`flex flex-col ${
-        isExpanded ? "xl:flex-col" : "xl:flex-row"
-      } justify-evenly items-center`}
-      >
-      <img className="w-1/3 h-1/3 rounded-xl" src={ProfilePicture} alt="" />
-      <div className="flex flex-col">
-        {isExpanded ? (
-          <div className="grid grid-cols-2 m-5">
-            <a
-              href={CV}
-              download
-              className="border-solid border-black border-4 rounded-xl"
-            >
-              <div className="flex flex-row">
-                <p>Download: </p>
-                <img
-                  src={DownloadIcon}
-                  className="w-20 h-9 mx-auto "
-                  href={CV}
-                  download
-                ></img>
-              </div>
-            </a>
-
-            <button
-              className="p-2 bg-red-500 text-white rounded-xl "
-              onClick={handleExpand}
-            >
-              Close
-            </button>
-          </div>
-        ) : (
-          <h3 className="Orbitron p-5 tracking-widest">CV:</h3>
-        )}
-
+    <>
+      {isExpanded ? (
         <div
-          className={`flex flex-col lg:flex-row cursor-pointer group ${
-            isExpanded ? " h-full w-full flex-col md:flex-col" : ""
-          }`}
+          className={`flex flex-col ${
+            isExpanded ? "xl:flex-col" : "xl:flex-row"
+          } justify-evenly items-center`}
         >
-          <Document file={CV} className="">
-            <Page
-              pageNumber={pageNumber}
-              scale={scale}
-              onClick={handleExpand}
-            />
-          </Document>
-          {!isExpanded ? (
-            <div className="flex flex-row lg:flex-col justify-evenly">
-              <img
-                src={PreviewIcon}
-                className="w-20 h-20 mx-5"
-                onClick={handleExpand}
-              ></img>
-              <a href={CV} download>
+          <img className="w-1/3 h-1/3 rounded-xl" src={ProfilePicture} alt="" />
+          <div className="flex flex-col">
+            <h3 className="Orbitron p-5 tracking-widest">CV:</h3>
+            <div
+              className={`flex flex-col lg:flex-row cursor-pointer group ${
+                isExpanded ? " h-full w-full flex-col md:flex-col" : ""
+              }`}
+            >
+              <Document file={CV} className="">
+                <Page
+                  pageNumber={pageNumber}
+                  scale={scale}
+                  onClick={handleExpand}
+                />
+              </Document>
+              <div className="flex flex-row lg:flex-col justify-evenly">
                 <img
-                  src={DownloadIcon}
+                  src={PreviewIcon}
                   className="w-20 h-20 mx-5"
-                  href={CV}
-                  download
+                  onClick={handleExpand}
                 ></img>
-              </a>
+                <a href={CV} download>
+                  <img
+                    src={DownloadIcon}
+                    className="w-20 h-20 mx-5"
+                    href={CV}
+                    download
+                  ></img>
+                </a>
+              </div>
             </div>
-          ) : (
-            <div className="grid grid-cols-2 place-items-center">
-              {pageNumber !== 1 && (
-                <img
-                  src={BackwardArrowIcon}
-                  className="m-5 w-20 h-14 col-start-1"
-                  onClick={() => setPageNumber(pageNumber - 1)}
-                  alt="Backward Arrow"
-                />
-              )}
-              {pageNumber !== 2 && (
-                <img
-                  src={ForwardArrowIcon}
-                  className="m-5 w-20 h-14 col-start-2"
-                  onClick={() => setPageNumber(pageNumber + 1)}
-                  alt="Forward Arrow"
-                />
-              )}
-            </div>
-          )}
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <EpandedPDF handleExpand={handleExpand}></EpandedPDF>
+      )}
+    </>
   );
 };
-
 export default PicCV;
--- src/components/expandedPDF.jsx
+++ src/components/expandedPDF.jsx
@@ -1,84 +1,82 @@
 import React, { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import DownloadIcon from "../assets/icons/Download_icon.svg";
+// import DownloadIcon from "../assets/icons/Download_icon.svg";
 import ForwardArrowIcon from "../assets/icons/Forward_arrow_icon.svg";
 import BackwardArrowIcon from "../assets/icons/Backward_arrow_icon.svg";
-// import CV from "../assets/CV23.pdf";
 import CV from "../assets/KV_CV23.pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const PicCV = () => {
+const ExpandedPDF = ({ handleExpand }) => {
   const [pageNumber, setPageNumber] = useState(1);
+  const [scale, setScale] = useState(window.innerWidth / 1000);
+
+  useEffect(() => {
+    // Update the scale whenever the window size changes
+    const handleResize = () => {
+      setScale(window.innerWidth / 1000);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); // Initialize scale on component mount
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [window.innerWidth]);
 
   return (
-    <div
-      className={`flex flex-col ${
-        isExpanded ? "xl:flex-col" : "xl:flex-row"
-      } justify-evenly items-center`}
-    >
-  
+    <div className={`flex flex-col xl:flex-col justify-evenly items-center`}>
       <div className="flex flex-col">
-          <div className="grid grid-cols-2 m-5">
-            <a
-              href={CV}
-              download
-              className="border-solid border-black border-4 rounded-xl"
-            >
-              <div className="flex flex-row">
-                <p>Download: </p>
-                <img
-                  src={DownloadIcon}
-                  className="w-20 h-9 mx-auto "
-                  href={CV}
-                  download
-                ></img>
-              </div>
-            </a>
+        <div className="grid grid-cols-2 m-5">
+          <button
+            className="p-2 border-solid border-black border-2 rounded-xl"
+            href={CV}
+            download
+            onClick={handleExpand}
+          >
+            Download
+          </button>
+          <button
+            className="p-2 bg-red-500 text-white rounded-xl "
+            onClick={handleExpand}
+          >
+            Close
+          </button>
+        </div>
 
-            <button
-              className="p-2 bg-red-500 text-white rounded-xl "
-              onClick={handleExpand}
-            >
-              Close
-            </button>
-          </div>
-
-        <div
-          className={`flex flex-col lg:flex-row cursor-pointer group ${
-            isExpanded ? " h-full w-full flex-col md:flex-col" : ""
-          }`}
-        >
+        <div className={`flex flex-col cursor-pointer group h-full w-full`}>
           <Document file={CV} className="">
             <Page
               pageNumber={pageNumber}
-              scale={window.innerWidth / 1000}
+              scale={scale}
               onClick={handleExpand}
             />
           </Document>
 
-            <div className="grid grid-cols-2 place-items-center">
-              {pageNumber !== 1 && (
-                <img
-                  src={BackwardArrowIcon}
-                  className="m-5 w-20 h-14 col-start-1"
-                  onClick={() => setPageNumber(pageNumber - 1)}
-                  alt="Backward Arrow"
-                />
-              )}
-              {pageNumber !== 2 && (
-                <img
-                  src={ForwardArrowIcon}
-                  className="m-5 w-20 h-14 col-start-2"
-                  onClick={() => setPageNumber(pageNumber + 1)}
-                  alt="Forward Arrow"
-                />
-              )}
-            </div>
+          <div className="grid grid-cols-2 place-items-center">
+            {pageNumber !== 1 && (
+              <img
+                src={BackwardArrowIcon}
+                className="m-5 w-20 h-14 col-start-1"
+                onClick={() => setPageNumber(pageNumber - 1)}
+                alt="Backward Arrow"
+              />
+            )}
+            {pageNumber !== 2 && (
+              <img
+                src={ForwardArrowIcon}
+                className="m-5 w-20 h-14 col-start-2"
+                onClick={() => setPageNumber(pageNumber + 1)}
+                alt="Forward Arrow"
+              />
+            )}
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default PicCV;
+export default ExpandedPDF;
+
